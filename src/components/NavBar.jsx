9ef82c3295aee743f495d56f91ce223487dd5376
Noemi <noemi.caggiano@gmail.com>
2023-04-25 11:14:19 +0100

    feat: navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,120 @@
+const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a href="#">Intro</a>
+            </li>
+            <li tabIndex={0}>
+              <a href="#" className="justify-between">
+                Projects
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2">
+                <li>
+                  <a>Cohort Manager 2.0</a>
+                </li>
+                <li>
+                  <a>RolePlay</a>
+                </li>
+                <li>
+                  <a>PlayNation</a>
+                </li>
+                <li>
+                  <a>Chat-App</a>
+                </li>
+                <li>
+                  <a>OrderDash</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Contacts</a>
+            </li>
+          </ul>
+        </div>
+        <a
+          className="btn btn-ghost normal-case text-xl"
+          href="https://www.linkedin.com/in/noemi-caggiano-19b924a4/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Noemi's Portfolio
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Intro</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Projects
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2">
+              <li>
+                <a>Cohort Manager 2.0</a>
+              </li>
+              <li>
+                <a>RolePlay</a>
+              </li>
+              <li>
+                <a>PlayNation</a>
+              </li>
+              <li>
+                <a>Chat-App</a>
+              </li>
+              <li>
+                <a>OrderDash</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Contacts</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end"></div>
+    </div>
+  );
+};
+
+export default NavBar;
