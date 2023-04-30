b824086ff962befcb1e6127a18af787930a3eb20
Noemi <noemi.caggiano@gmail.com>
2023-05-01 00:30:45 +0100

    feat: hero layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,38 +2,52 @@ import { projects } from "../utils/utils";
 
 const Projects = () => {
   return (
-    <div id="projects" className="mb-2.5 flex flex-col justify-center">
+    <div id="projects" className="mb-2.5">
       <div className="carousel">
         {projects.map((proj, i) => {
           return (
-            <div id={`item${i + 1}`} className="carousel-item w-full mb-8">
+            <div
+              id={`item${i + 1}`}
+              className="carousel-item justify-center w-full mb-8"
+            >
               <div className="card w-3/5 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
                   <img src={proj.img} alt={proj.name} className="rounded-xl" />
                 </figure>
-                <div className="card-body items-center text-center">
+                <div className="card-body justify-center items-center text-center">
                   <h2 className="card-title">{proj.name}</h2>
                   <p>{proj.intro}</p>
-                  <div className="card-actions flex justify-center">
+                  <div className="card-actions">
                     {typeof proj.repo !== "object" ? (
-                      <button className="btn btn-primary sm:btn-sm md:btn-md lg:btn-lg">
-                        <a href={proj.repo} target="_blank" rel="noreferrer">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                          </svg>
-                          Repository
-                        </a>
-                      </button>
+                      <a
+                        href={proj.repo}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="btn btn-primary btn-sm md:btn-md gap-1 md:gap-3"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                        </svg>
+                        <span className="text-xs md:text-sm">Repository</span>
+                      </a>
                     ) : (
-                      proj.repo.map((r) => {
+                      proj.repo.map((r, i) => {
                         return (
-                          <button className="btn btn-primary sm:btn-sm md:btn-md lg:btn-lg">
-                            <a href={r} target="_blank" rel="noreferrer">
+                          <div
+                            className="tooltip"
+                            data-tip={i === 0 ? "Client Side" : "Server Side"}
+                          >
+                            <a
+                              href={r}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="btn btn-primary md:btn-md btn-sm gap-1 md:gap-3"
+                            >
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -42,11 +56,34 @@ const Projects = () => {
                               >
                                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                               </svg>
-                              Repository
+                              <span className="text-xs md:text-sm">
+                                Repository
+                              </span>
                             </a>
-                          </button>
+                          </div>
                         );
                       })
+                    )}
+                    {proj.liveApp && (
+                      <div className="tooltip" data-tip="Live Application">
+                        <a
+                          href={proj.liveApp}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="btn btn-primary md:btn-md btn-sm gap-1 md:gap-3"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            class="w-6 h-6 fill-black"
+                          >
+                            <path d="M21.721 12.752a9.711 9.711 0 00-.945-5.003 12.754 12.754 0 01-4.339 2.708 18.991 18.991 0 01-.214 4.772 17.165 17.165 0 005.498-2.477zM14.634 15.55a17.324 17.324 0 00.332-4.647c-.952.227-1.945.347-2.966.347-1.021 0-2.014-.12-2.966-.347a17.515 17.515 0 00.332 4.647 17.385 17.385 0 005.268 0zM9.772 17.119a18.963 18.963 0 004.456 0A17.182 17.182 0 0112 21.724a17.18 17.18 0 01-2.228-4.605zM7.777 15.23a18.87 18.87 0 01-.214-4.774 12.753 12.753 0 01-4.34-2.708 9.711 9.711 0 00-.944 5.004 17.165 17.165 0 005.498 2.477zM21.356 14.752a9.765 9.765 0 01-7.478 6.817 18.64 18.64 0 001.988-4.718 18.627 18.627 0 005.49-2.098zM2.644 14.752c1.682.971 3.53 1.688 5.49 2.099a18.64 18.64 0 001.988 4.718 9.765 9.765 0 01-7.478-6.816zM13.878 2.43a9.755 9.755 0 016.116 3.986 11.267 11.267 0 01-3.746 2.504 18.63 18.63 0 00-2.37-6.49zM12 2.276a17.152 17.152 0 012.805 7.121c-.897.23-1.837.353-2.805.353-.968 0-1.908-.122-2.805-.353A17.151 17.151 0 0112 2.276zM10.122 2.43a18.629 18.629 0 00-2.37 6.49 11.266 11.266 0 01-3.746-2.504 9.754 9.754 0 016.116-3.985z" />
+                          </svg>
+
+                          <span className="text-xs md:text-sm">Live App</span>
+                        </a>
+                      </div>
                     )}
                   </div>
                 </div>
@@ -55,7 +92,7 @@ const Projects = () => {
           );
         })}
       </div>
-      <div className="flex justify-center w-full py-2 gap-2">
+      <div className="flex justify-center py-2 gap-2">
         {projects.map((p, i) => {
           return (
             <a href={`#item${i + 1}`} className="btn btn-xs">
